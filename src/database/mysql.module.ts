d0bebd5ad5834d9823aab77beccbb1d53891a328
knowave <kwaktaemin98@gmail.com>
2024-12-03 10:23:28 +0700

    entity

---
+++ src/database/mysql.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '../../domains/**/entities/**.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*.ts'],
+        synchronize: true,
+        migrationsRun: true,
+      }),
+
+      dataSourceFactory: async (options) => {
+        try {
+          const dataSource = await new DataSource(options).initialize();
+          console.log('Data Source has been initialized!');
+          return dataSource;
+        } catch (err) {
+          console.log('Error establishing database connection:', err);
+        }
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
---
+++ src/domains/user/entities/user.entity.ts
@@ -0,0 +1,5 @@
+import { BaseEntity } from 'src/common/base.entity';
+import { Entity } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {}
