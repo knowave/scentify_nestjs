e28b895d1e9ecf9975a8bbb58f73780f347cc667
knowave <kwaktaemin98@gmail.com>
2024-12-03 15:11:17 +0700

    add user, perfume, auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MysqlModule } from './database/mysql.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './domains/user/user.module';
+import { PerfumeModule } from './domains/perfume/perfume.module';
+import { AuthModule } from './domains/auth/auth.module';
 
 @Module({
   imports: [
@@ -9,6 +12,9 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
     }),
     MysqlModule,
+    UserModule,
+    PerfumeModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
