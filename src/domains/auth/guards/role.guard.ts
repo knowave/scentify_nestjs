06cdbcc20587d82fb27631ed68b6f28ef9e48a94
knowave <kwaktaemin98@gmail.com>
2024-12-03 17:13:13 +0700

    role guard

---
+++ src/domains/auth/guards/role.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from 'src/common/decorators/role.decorator';
+import { Role } from 'src/common/enums/role.enum';
+
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requireRole = this.reflector.get<Role[]>(
+      ROLES_KEY,
+      context.getHandler(),
+    );
+
+    if (!requireRole) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+
+    if (!user) return false;
+
+    const userRole = user.role;
+    return requireRole.includes(userRole);
+  }
+}
