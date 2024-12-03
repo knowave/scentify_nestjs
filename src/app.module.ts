b94e0d4c0798d60a7c7745803cd8f8e188b1fe43
knowave <kwaktaemin98@gmail.com>
2024-12-03 10:41:25 +0700

    add configmodule, mysqlmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MysqlModule } from './database/mysql.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MysqlModule,
+  ],
   controllers: [],
   providers: [],
 })
