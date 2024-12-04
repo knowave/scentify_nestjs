6014bacbecd82693b5b930830556be56cc438e7b
knowave <kwaktaemin98@gmail.com>
2024-12-04 12:42:59 +0700

    user error code

---
+++ src/domains/user/error/error-code.ts
@@ -0,0 +1,9 @@
+export const NOT_FOUND_USER = {
+  code: 'NOT_FOUND_USER',
+  message: 'Not found user.',
+};
+
+export const UNAUTHORIZED_USER = {
+  code: 'UNAUTHORIZED_USER',
+  message: 'Unauthorized user',
+};
