3c30bd4346c4712661662616a234d914f87e890f
knowave <kwaktaemin98@gmail.com>
2024-12-04 12:43:56 +0700

    passport, jwt 설정

--- src/domains/auth/auth.module.ts
+++ src/domains/auth/auth.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import {
+  JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+  JWT_ACCESS_TOKEN_SECRET,
+} from 'src/common/env';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { NaverStrategy } from './strategies/naver.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: JWT_ACCESS_TOKEN_SECRET,
+      signOptions: {
+        expiresIn: `${JWT_ACCESS_TOKEN_EXPIRATION_TIME}`,
+      },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, NaverStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
