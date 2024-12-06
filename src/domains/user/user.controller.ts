77c897eea26eff42a93089aaca7edd46454df5b1
knowave <kwaktaemin98@gmail.com>
2024-12-06 15:59:46 +0700

    내 정보 조회

--- src/domains/user/user.controller.ts
+++ src/domains/user/user.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Public } from 'src/common/decorators/public.decorator';
+import { CurrentUser } from 'src/common/decorators/user.decorator';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -11,5 +13,11 @@ export class UserController {
   @Post('/sign-up')
   async createUser(@Body() createUserDto: CreateUserDto): Promise<void> {
     return await this.userService.createUser(createUserDto);
+  }
+
+  @Public()
+  @Get('/:id')
+  async getUserById(@CurrentUser() user: User): Promise<User> {
+    return await this.userService.getUserById(user.id);
   }
 }
