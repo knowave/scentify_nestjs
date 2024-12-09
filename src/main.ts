4670d94f6482223d07679192a98cfb5d75737f7b
knowave <kwaktaemin98@gmail.com>
2024-12-09 13:19:44 +0700

    ejs 템플릿을 활용한 소셜 로그인 테스트 진행

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.setBaseViewsDir(join(__dirname, '../src', 'views'));
+  app.useStaticAssets(join(__dirname, '../src', 'public'));
+  app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
