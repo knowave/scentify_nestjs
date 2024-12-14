e998513d20b263900569c9ae1c78005b04ba3a97
knowave <kwaktaemin98@gmail.com>
2024-12-14 15:35:56 +0700

    UserModule import

--- src/domains/perfume/perfume.module.ts
+++ src/domains/perfume/perfume.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Perfume } from './entities/perfume.entity';
 import { PerfumeRepository } from './perfume.repository';
 import { S3Module } from '../s3/s3.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Perfume]), S3Module],
+  imports: [TypeOrmModule.forFeature([Perfume]), S3Module, UserModule],
   providers: [PerfumeService, PerfumeRepository],
   controllers: [PerfumeController],
 })
